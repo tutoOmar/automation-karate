054786f9f5ec87c977b2b8346d193f11fa85a705
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-04-04 18:54:26 -0500

    Feature adding new test cases for products method GET

--- src/test/java/examples/products/ProductsRunner.java
+++ src/test/java/examples/products/ProductsRunner.java
@@ -1,4 +1,10 @@
 package examples.products;
 
+import com.intuit.karate.junit5.Karate;
+
 public class ProductsRunner {
+    @Karate.Test
+    Karate getProducts(){
+        return Karate.run("products").relativeTo(getClass());
+    }
 }
