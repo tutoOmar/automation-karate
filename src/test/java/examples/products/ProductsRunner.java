f794c792e705e912407210ef9fdcb20b14850da7
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-04-04 18:54:20 -0500

    Feature: Creating first products feature

--- src/test/java/examples/products/ProductsRunner.java
+++ src/test/java/examples/products/ProductsRunner.java
@@ -1,12 +1,4 @@
 package examples.products;
 
-import com.intuit.karate.junit5.Karate;
-
-class ProductsRunner {
-
-    @Karate.Test
-    Karate testProducts() {
-        return Karate.run("products").relativeTo(getClass());
-    }
-
+public class ProductsRunner {
 }
